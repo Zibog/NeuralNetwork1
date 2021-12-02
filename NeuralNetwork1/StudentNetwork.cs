6f81148e6939c5a730d76f78a049602fc002489f
Zibog <Zibog@github.com>
2021-12-02 22:18:27 +0300

    Optimize training

--- NeuralNetwork1/StudentNetwork.cs
+++ NeuralNetwork1/StudentNetwork.cs
@@ -11,12 +11,12 @@ namespace NeuralNetwork1
         /// </summary>
         private class Neuron
         {
-            public double output = 0; //выходной сигнал
-            public double error = 0; //ошибка
-            public double[] inputWeights; //входные веса
-            public Neuron[] prevLayer; //ссылка на предыдущий слой
+            public double Output; //выходной сигнал
+            public double Error; //ошибка
+            private double[] _inputWeights; //входные веса
+            private Neuron[] _prevLayer; //ссылка на предыдущий слой
             
-            static Random random = new Random(); //генератор случайных чисел
+            private static Random _rand = new Random(); //генератор случайных чисел
 
             /// <summary>
             /// Создание нейрона первого слоя (сенсора)
@@ -29,51 +29,51 @@ namespace NeuralNetwork1
             /// <param name="neurons">Ссылка на предыдущий слой</param>
             public Neuron(Neuron[] neurons)
             {
-                prevLayer = neurons;
-                inputWeights = new double[prevLayer.Length];
+                _prevLayer = neurons;
+                _inputWeights = new double[_prevLayer.Length];
                 //заполняем входные веса случайным образом от -1 до 1
-                for (int i = 0; i < prevLayer.Length; i++)
-                    inputWeights[i] = -1 + random.NextDouble() * 2;
+                for (int i = 0; i < _prevLayer.Length; i++)
+                    _inputWeights[i] = -1 + _rand.NextDouble() * 2;
             }
 
             /// <summary>
             /// Вычисление передаточной функции
             /// </summary>
-            public void CalcGearFunction()
+            public void Activate()
             {
                 double weight = 0;
-                for (int i = 0; i < prevLayer.Length; i++)
-                    weight += prevLayer[i].output * inputWeights[i];
+                for (int i = 0; i < _prevLayer.Length; i++)
+                    weight += _prevLayer[i].Output * _inputWeights[i];
 
-                output = 1 / (1 + Math.Exp(-weight)); //сигмоидальная передаточная функция
+                Output = 1 / (1 + Math.Exp(-weight)); //сигмоидальная передаточная функция
             }
 
-            public void BackError()
+            public void ErrorBackPropagation()
             {
                 //обрабатываем ошибку в текущем нейроне
-                error *= output * (1 - output);
+                Error *= Output * (1 - Output);
 
                 //переносим ошибку на предыдущий слой
-                for (int i = 0; i < prevLayer.Length; i++)
-                    prevLayer[i].error += error * inputWeights[i];
+                for (int i = 0; i < _prevLayer.Length; i++)
+                    _prevLayer[i].Error += Error * _inputWeights[i];
 
                 //корректируем веса
-                for (int i = 0; i < prevLayer.Length; i++)
-                    inputWeights[i] += error * prevLayer[i].output;
+                for (int i = 0; i < _prevLayer.Length; i++)
+                    _inputWeights[i] += Error * _prevLayer[i].Output;
 
-                error = 0;
+                Error = 0;
             }
         }
 
-        private List<List<Neuron>> neuronsLayers = new List<List<Neuron>>(); //список слоев сети
+        private List<Neuron[]> neuronsLayers = new List<Neuron[]>(); //список слоев сети
 
         public StudentNetwork(int[] structure)
         {
             for (int i = 0; i < structure.Length; i++)
             {
-                List<Neuron> neurons = new List<Neuron>(); //создаем слой сети
+                var neurons = new Neuron[structure[i]]; //создаем слой сети
                 for (int j = 0; j < structure[i]; j++)
-                    neurons.Add(i == 0 ? new Neuron() : new Neuron(neuronsLayers[i - 1].ToArray())); //создаем нейроны
+                    neurons[j] = i == 0 ? new Neuron() : new Neuron(neuronsLayers[i - 1]); //создаем нейроны
                 neuronsLayers.Add(neurons);
             }
         }
@@ -84,89 +84,104 @@ namespace NeuralNetwork1
         private void Forward(Sample sample)
         {
             for (int i = 0; i < sample.input.Length; i++)
-                neuronsLayers[0][i].output = sample.input[i]; //переносим значения на сенсоры
+                neuronsLayers[0][i].Output = sample.input[i]; //переносим значения на сенсоры
 
             //Выполняем послойно вычисления передаточных функций
             for (int i = 1; i < neuronsLayers.Count; i++)
                 foreach (var neuron in neuronsLayers[i])
-                    neuron.CalcGearFunction();
+                    neuron.Activate();
 
-            for (int i = 0; i < neuronsLayers.Last().Count; i++)
-                sample.Output[i] = neuronsLayers.Last()[i].output; //переносим значения с последнего слоя в вектор значений изображения
+            for (int i = 0; i < neuronsLayers.Last().Length; i++)
+                sample.Output[i] = neuronsLayers.Last()[i].Output; //переносим значения с последнего слоя в вектор значений изображения
 
-            sample.recognizedClass = Predict(sample);
+            //sample.recognizedClass = Predict(sample);
+            sample.recognizedClass =
+                sample.ProcessPrediction(sample.Output);
+        }
+
+        private void Forward(double[] input)
+        {
+            for (int i = 0; i < input.Length; i++)
+                neuronsLayers[0][i].Output = input[i]; //переносим значения на сенсоры
+
+            //Выполняем послойно вычисления передаточных функций
+            for (int i = 1; i < neuronsLayers.Count; i++)
+                foreach (var neuron in neuronsLayers[i])
+                    neuron.Activate();
         }
 
         /// <summary>
-        /// Запускаем алгоритм обратного распространения ошибки
+        /// Алгоритм обратного распространения ошибки
         /// </summary>
-        void BackError(Sample sample)
+        void ErrorBackPropagation(Sample sample)
         {
             //переносим ошибку из картинки на последний слой нейронов
-            for (int i = 0; i < neuronsLayers.Last().Count; i++)
-                neuronsLayers.Last()[i].error = sample.error[i];
+            for (int i = 0; i < neuronsLayers.Last().Length; i++)
+                neuronsLayers.Last()[i].Error = sample.error[i];
 
             //переносим ошибки от слоя к слою
             for (int i = neuronsLayers.Count - 1; i >= 1; i--)
                 foreach (var neuron in neuronsLayers[i])
-                    neuron.BackError();
+                    neuron.ErrorBackPropagation();
         }
 
         public override int Train(Sample sample, double acceptableError, bool parallel)
         {
-            int cnt = 0;
+            int i = 0;
             //запускаем сеть на примере до тех пор, пока правильно не распознается (максимум 100 раз)
-            while (cnt < 100)
+            while (i < 100)
             {
                 Forward(sample);
 
-                if (sample.EstimatedError() < acceptableError && sample.Correct()) return cnt;
+                if (sample.EstimatedError() < acceptableError && sample.Correct()) 
+                    return i;
 
                 //если мы здесь, значит ошибка распознавания (или слишком большая ошибка), запускаем алгоритм обратного распространения ошибки
-                cnt++;
-                BackError(sample);
+                i++;
+                ErrorBackPropagation(sample);
             }
-            return cnt;
+            return i;
         }
 
         public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError, bool parallel)
         {
-            var t = System.DateTime.Now;
+            var startTime = DateTime.Now;
             double accuracy = 0;
-
-            int samplesLooked = 0; //сколько всего элементов было рассмотрено
-            double allSamplesCount = samplesSet.samples.Count * epochsCount; //сколько всего элементов нужно будет рассмотреть
+            var samplesLooked = 0; //сколько всего элементов было рассмотрено
+            double samplesCount = samplesSet.samples.Count * epochsCount; //сколько всего элементов нужно будет рассмотреть
 
             while (epochsCount-- > 0)
             {
-                double rightCnt = 0;
+                double correctAnswers = 0;
                 //перебираем весь датасет
                 foreach (var sample in samplesSet.samples)
                 {
-                    if (Train(sample, acceptableError, parallel) == 0) rightCnt++;
+                    if (Train(sample, acceptableError, parallel) == 0) 
+                        correctAnswers++;
                     samplesLooked++;
                     if (samplesLooked % 25 == 0) //каждые 25 рассмотренных примеров обновляем индикатор прогресса
-                        OnTrainProgress(samplesLooked / allSamplesCount, accuracy, DateTime.Now - t);
+                        OnTrainProgress(samplesLooked / samplesCount, accuracy, DateTime.Now - startTime);
                 }
 
 
-                accuracy = rightCnt / samplesSet.samples.Count; //после конца каждой эпохи перессчитываем точность для передачи ее в форму
+                accuracy = correctAnswers / samplesSet.samples.Count; //после конца каждой эпохи перессчитываем точность для передачи ее в форму
                 if (accuracy >= 1 - acceptableError - 1e-10) //если точность соответствует допустимой ошибке, выходим
                 {
-                    OnTrainProgress(1, accuracy, DateTime.Now - t);
+                    OnTrainProgress(1, accuracy, DateTime.Now - startTime);
                     return accuracy;
                 }
 
-               OnTrainProgress(samplesLooked / allSamplesCount, accuracy, DateTime.Now - t);
+                OnTrainProgress(samplesLooked / samplesCount, accuracy, DateTime.Now - startTime);
             }
 
-            OnTrainProgress(1, accuracy, DateTime.Now - t);
+            OnTrainProgress(1, accuracy, DateTime.Now - startTime);
             return accuracy;
         }
 
         protected override double[] Compute(double[] input)
         {
-            return neuronsLayers.Last().Select(neuron => neuron.output).ToArray();
+            Forward(input);
+            return neuronsLayers.Last().Select(neuron => neuron.Output).ToArray();
         }
     }
 }
