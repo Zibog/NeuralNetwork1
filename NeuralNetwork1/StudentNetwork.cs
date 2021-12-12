b9c229dfa1a27f663c52fff7d84ef87905a71386
Zibog <dmsidak@gmail.com>
2021-12-12 12:22:24 +0300

    Refactor functions

--- NeuralNetwork1/StudentNetwork.cs
+++ NeuralNetwork1/StudentNetwork.cs
@@ -7,83 +7,68 @@ namespace NeuralNetwork1
 {
     public class StudentNetwork : BaseNetwork
     {
-        private readonly double [][,] _weights;
+        private readonly double[][,] _weights;
         private readonly double[][] _values;
         private readonly double[][] _errors;
         private const double Speed = 0.25;
         private readonly Stopwatch _stopWatch = new Stopwatch();
 
-        public StudentNetwork(int [] structure, double lowRandomBound=-1, double upRandomBound=1)
+        public StudentNetwork(int[] structure, double lowerBound = -1, double upperBound = 1)
         {
             _values = new double[structure.Length][];
             _errors = new double[structure.Length][];
             for (int i = 0; i < structure.Length; i++)
             {
                 _errors[i] = new double[structure[i]];
-                _values[i] = new double[structure[i]+1];
+                _values[i] = new double[structure[i] + 1];
                 _values[i][structure[i]] = 1;
             }
             _weights = new double[structure.Length - 1][,];
-            for(int n = 0; n < structure.Length-1; n++)
+            for(int n = 0; n < structure.Length - 1; n++)
             {
                 var rowsCount = structure[n]+1;
                 var columnsCount = structure[n+1];
                 _weights[n] = new double[rowsCount, columnsCount];
-                Random random = new Random();
-                for(int i = 0; i < rowsCount; i++)
-                {
-                    for(int j=0; j<columnsCount; j++)
-                    {
-                        _weights[n][i, j] = random.NextDouble() * (upRandomBound - lowRandomBound) + lowRandomBound;
-                    }
-                }
+                var r = new Random();
+                for (int i = 0; i < rowsCount; i++)
+                    for (int j = 0; j < columnsCount; j++)
+                        _weights[n][i, j] = r.NextDouble() * (upperBound - lowerBound) + lowerBound;
             }
         }
 
         private double Error(double[] output)
         {
             double result = 0;
-            for(int i = 0; i < output.Length; i++)
-            {
-                result += (Math.Pow(output[i] - _values[_values.Length - 1][i], 2));
-            }
+            for (int i = 0; i < output.Length; i++) 
+                result += Math.Pow(output[i] - _values[_values.Length - 1][i], 2);
             result /= output.Length;
             return result;
         }
 
-        private void ComputeValues(double[] input)
+        private void Run(double[] input)
         {
-            for(int j = 0; j < input.Length; j++)
-            {
+            for (int j = 0; j < input.Length; j++) 
                 _values[0][j] = input[j];
-            }
-            for(int i=1; i < _values.GetLength(0); i++)
-            {
-                MultiplyAndApplySigmoid(_values[i - 1], _weights[i-1], _values[i]);
-            }
+            for (int i = 1; i < _values.GetLength(0); i++)
+                MultiplyAndApplySigmoid(_values[i - 1], _weights[i - 1], _values[i]);
         }
 
         private static void MultiplyAndApplySigmoid(double[] vector, double [,] matrix, double[] result)
         {
             var rowsCount = matrix.GetLength(0);
             var colCount = matrix.GetLength(1);
-            for (int i = 0; i <colCount ; i++)
+            for (int i = 0; i < colCount ; i++)
             {
                 double sum = 0;
-                for(int j = 0; j <rowsCount; j++)
-                {
+                for(int j = 0; j < rowsCount; j++) 
                     sum += vector[j] * matrix[j, i];
-                }
                 result[i] = Sigmoid(sum);
             }  
         }
 
-        private static double Sigmoid(double value)
-        {
-            return 1.0 / (Math.Exp(-value) + 1);
-        }
+        private static double Sigmoid(double value) => 1.0 / (Math.Exp(-value) + 1);
 
-        private void ComputeErrors(double[] output)
+        private void BackPropagation(double[] output)
         {
             for (var j = 0; j < output.Length; j++)
             {
@@ -92,19 +77,15 @@ namespace NeuralNetwork1
                 _errors[_errors.Length - 1][j] = actualValue * (1 - actualValue) * (expectedValue - actualValue);
             }
             for (int i = _errors.Length - 2; i >= 1; i--)
-            {
                 for (int j = 0; j < _errors[i].Length; j++)
                 {
                     var value = _values[i][j];
-                    value = value * (1 - value);
+                    value *= 1 - value;
                     var sum = 0.0;
                     for (int k = 0; k < _errors[i + 1].Length; k++)
-                    {
                         sum += _errors[i + 1][k] * _weights[i][j, k];
-                    }
                     _errors[i][j] = value * sum;
                 }
-            }
         }
 
         private void ComputeWeights()
@@ -125,16 +106,16 @@ namespace NeuralNetwork1
 
         public override int Train(Sample sample, double acceptableError, bool parallel)
         {
-            int iters = 1;
-            while (Error(sample.Output) > acceptableError)
+            int i = 1;
+            while (sample.EstimatedError() > acceptableError)
             {
-                ComputeValues(sample.input);
-                ComputeErrors(sample.Output);
+                Run(sample.input);
+                BackPropagation(sample.Output);
                 ComputeWeights();
-                ++iters;
+                ++i;
             }
 
-            return iters;
+            return i;
         }
 
         public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError, bool parallel)
@@ -161,14 +142,14 @@ namespace NeuralNetwork1
 
             _stopWatch.Restart();
 
-            while (epochToRun < epochsCount && error > acceptableError)
+            while (epochToRun++ < epochsCount && error > acceptableError)
             {
-                epochToRun++;
+                //epochToRun++;
                 error = 0;
                 for (int i = 0; i < inputs.Length; i++)
                 {
-                    ComputeValues(inputs[i]);
-                    ComputeErrors(outputs[i]);
+                    Run(inputs[i]);
+                    BackPropagation(outputs[i]);
                     ComputeWeights();
                     error += Error(outputs[i]);
                 }
@@ -187,7 +168,7 @@ namespace NeuralNetwork1
 
         protected override double[] Compute(double[] input)
         {
-            ComputeValues(input);
+            Run(input);
             return _values.Last().Take(_values.Last().Length - 1).ToArray();
         }
     }
