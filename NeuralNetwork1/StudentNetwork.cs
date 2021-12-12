363018daf56b5782d2d004a280e4b70169aabe2e
Zibog <dmsidak@gmail.com>
2021-12-12 13:09:22 +0300

    Fix counter, add comments

--- NeuralNetwork1/StudentNetwork.cs
+++ NeuralNetwork1/StudentNetwork.cs
@@ -7,87 +7,135 @@ namespace NeuralNetwork1
 {
     public class StudentNetwork : BaseNetwork
     {
+        /// <summary>
+        /// Массив входных матриц весов
+        /// </summary>
         private readonly double[][,] _weights;
-        private readonly double[][] _values;
+        
+        /// <summary>
+        /// Входной взвешенный сигнал
+        /// </summary>
+        private readonly double[][] _charges;
+        
+        /// <summary>
+        /// Значение ошибки
+        /// </summary>
         private readonly double[][] _errors;
+        
+        /// <summary>
+        /// Скорость обучения
+        /// </summary>
         private const double Speed = 0.25;
+        
         private readonly Stopwatch _stopWatch = new Stopwatch();
 
         public StudentNetwork(int[] structure, double lowerBound = -1, double upperBound = 1)
         {
-            _values = new double[structure.Length][];
+            _charges = new double[structure.Length][];
             _errors = new double[structure.Length][];
+            
             for (int i = 0; i < structure.Length; i++)
             {
                 _errors[i] = new double[structure[i]];
-                _values[i] = new double[structure[i] + 1];
-                _values[i][structure[i]] = 1;
+                _charges[i] = new double[structure[i] + 1];
+                _charges[i][structure[i]] = 1;
             }
+            
             _weights = new double[structure.Length - 1][,];
+            
             for(int n = 0; n < structure.Length - 1; n++)
             {
-                var rowsCount = structure[n]+1;
-                var columnsCount = structure[n+1];
-                _weights[n] = new double[rowsCount, columnsCount];
                 var r = new Random();
+                var rowsCount = structure[n] + 1;
+                var columnsCount = structure[n + 1];
+                
+                _weights[n] = new double[rowsCount, columnsCount];
+                
                 for (int i = 0; i < rowsCount; i++)
                     for (int j = 0; j < columnsCount; j++)
-                        _weights[n][i, j] = r.NextDouble() * (upperBound - lowerBound) + lowerBound;
+                        _weights[n][i, j] = lowerBound + r.NextDouble() * (upperBound - lowerBound);
             }
         }
 
+        /// <summary>
+        /// Вычисление квадратичной ошибки по выходу сети, аналог sample.EstimatedError для массива
+        /// </summary>
         private double Error(double[] output)
         {
             double result = 0;
+            
             for (int i = 0; i < output.Length; i++) 
-                result += Math.Pow(output[i] - _values[_values.Length - 1][i], 2);
+                result += Math.Pow(output[i] - _charges[_charges.Length - 1][i], 2);
+            
             result /= output.Length;
+            
             return result;
         }
 
+        /// <summary>
+        /// Прямой проход сети
+        /// </summary>
         private void Run(double[] input)
         {
             for (int j = 0; j < input.Length; j++) 
-                _values[0][j] = input[j];
-            for (int i = 1; i < _values.GetLength(0); i++)
-                MultiplyAndApplySigmoid(_values[i - 1], _weights[i - 1], _values[i]);
+                _charges[0][j] = input[j];
+            
+            for (int i = 1; i < _charges.GetLength(0); i++)
+                MultiplyAndApplySigmoid(_charges[i - 1], _weights[i - 1], _charges[i]);
         }
 
+        /// <summary>
+        /// Функция активации слоя сети
+        /// </summary>
         private static void MultiplyAndApplySigmoid(double[] vector, double [,] matrix, double[] result)
         {
             var rowsCount = matrix.GetLength(0);
             var colCount = matrix.GetLength(1);
+            
             for (int i = 0; i < colCount ; i++)
             {
                 double sum = 0;
+                
                 for(int j = 0; j < rowsCount; j++) 
                     sum += vector[j] * matrix[j, i];
+                
                 result[i] = Sigmoid(sum);
             }  
         }
 
+        /// <summary>
+        /// Сигмоида, активирующая "нейрон"
+        /// </summary>
         private static double Sigmoid(double value) => 1.0 / (Math.Exp(-value) + 1);
 
+        /// <summary>
+        /// Обратное распространение ошибки
+        /// </summary>
         private void BackPropagation(double[] output)
         {
+            // Обработка ошибки
             for (var j = 0; j < output.Length; j++)
             {
-                var actualValue = _values[_errors.Length - 1][j];
-                var expectedValue = output[j];
-                _errors[_errors.Length - 1][j] = actualValue * (1 - actualValue) * (expectedValue - actualValue);
+                var currentCharge = _charges[_errors.Length - 1][j];
+                var expectedOutput = output[j];
+                _errors[_errors.Length - 1][j] = currentCharge * (1 - currentCharge) * (expectedOutput - currentCharge);
             }
+            // Пересчёт ошибки
             for (int i = _errors.Length - 2; i >= 1; i--)
                 for (int j = 0; j < _errors[i].Length; j++)
                 {
-                    var value = _values[i][j];
-                    value *= 1 - value;
+                    var charge = _charges[i][j];
+                    charge *= 1 - charge;
                     var sum = 0.0;
                     for (int k = 0; k < _errors[i + 1].Length; k++)
                         sum += _errors[i + 1][k] * _weights[i][j, k];
-                    _errors[i][j] = value * sum;
+                    _errors[i][j] = charge * sum;
                 }
         }
 
+        /// <summary>
+        /// Пересчёт весов
+        /// </summary>
         private void ComputeWeights()
         {
            
@@ -97,7 +145,7 @@ namespace NeuralNetwork1
                 {
                     for(int j = 0; j < _weights[n].GetLength(1); j++)
                     {
-                        var dWeight = Speed * _errors[n + 1][j] * _values[n][i];
+                        var dWeight = Speed * _errors[n + 1][j] * _charges[n][i];
                         _weights[n][i, j] += dWeight;
                     }
                 }
@@ -120,20 +168,20 @@ namespace NeuralNetwork1
 
         public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError, bool parallel)
         {
-            //  Сначала надо сконструировать массивы входов и выходов
+            // Конструируем массивы входов и выходов
             double[][] inputs = new double[samplesSet.Count][];
             double[][] outputs = new double[samplesSet.Count][];
 
-            //  Теперь массивы из samplesSet группируем в inputs и outputs
+            // Группируем массивы из samplesSet в inputs и outputs
             for (int i = 0; i < samplesSet.Count; ++i)
             {
                 inputs[i] = samplesSet[i].input;
                 outputs[i] = samplesSet[i].Output;
             }
 
-            //  Текущий счётчик эпох
             int epochToRun = 0;
-
+            double samplesLooked = 0;
+            double samplesCount = inputs.Length * epochsCount;
             double error = double.PositiveInfinity;
 
 #if DEBUG
@@ -144,7 +192,6 @@ namespace NeuralNetwork1
 
             while (epochToRun++ < epochsCount && error > acceptableError)
             {
-                //epochToRun++;
                 error = 0;
                 for (int i = 0; i < inputs.Length; i++)
                 {
@@ -152,16 +199,17 @@ namespace NeuralNetwork1
                     BackPropagation(outputs[i]);
                     ComputeWeights();
                     error += Error(outputs[i]);
+                    ++samplesLooked;
                 }
                 error /= inputs.Length;
                 errorsFile.WriteLine(error);
-                OnTrainProgress(epochToRun * 1.0 / epochsCount, error, _stopWatch.Elapsed);
+                OnTrainProgress(samplesLooked / samplesCount, error, _stopWatch.Elapsed);
             }
 
 #if DEBUG
             errorsFile.Close();
 #endif
-            OnTrainProgress(epochToRun * 1.0 / epochsCount, error, _stopWatch.Elapsed);
+            OnTrainProgress(1, error, _stopWatch.Elapsed);
             _stopWatch.Stop();
             return error;
         }
@@ -169,7 +217,7 @@ namespace NeuralNetwork1
         protected override double[] Compute(double[] input)
         {
             Run(input);
-            return _values.Last().Take(_values.Last().Length - 1).ToArray();
+            return _charges.Last().Take(_charges.Last().Length - 1).ToArray();
         }
     }
 }
