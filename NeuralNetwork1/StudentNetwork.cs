9104ce3be16e49525ad77edd177cc22c68e3139f
Zibog <dmsidak@gmail.com>
2021-12-12 12:09:46 +0300

    Re-re-rewrite entire network, now it works only with arrays

--- NeuralNetwork1/StudentNetwork.cs
+++ NeuralNetwork1/StudentNetwork.cs
@@ -1,254 +1,194 @@
 ﻿using System;
+using System.Diagnostics;
+using System.IO;
 using System.Linq;
-using System.Threading.Tasks;
 
 namespace NeuralNetwork1
 {
     public class StudentNetwork : BaseNetwork
     {
-        /// <summary>
-        /// Один нейрон сети
-        /// </summary>
-        private class Node
+        private readonly double [][,] _weights;
+        private readonly double[][] _values;
+        private readonly double[][] _errors;
+        private const double Speed = 0.25;
+        private readonly Stopwatch _stopWatch = new Stopwatch();
+
+        public StudentNetwork(int [] structure, double lowRandomBound=-1, double upRandomBound=1)
         {
-            /// <summary>
-            /// Входной взвешенный сигнал нейрона
-            /// </summary>
-            private double _charge;
-
-            /// <summary>
-            /// Выходной сигнал нейрона
-            /// </summary>
-            public double output;
-
-            /// <summary>
-            /// Ошибка для данного нейрона
-            /// </summary>
-            public double error;
-
-            /// <summary>
-            /// Сигнал поляризации
-            /// </summary>
-            private const double BiasSignal = -1.0;
-
-            /// <summary>
-            /// Генератор для инициализации весов
-            /// </summary>
-            private static Random _r = new Random();
-
-            /// <summary>
-            /// Минимальное значение для начальной инициализации весов
-            /// </summary>
-            private static double _initMinWeight = -1;//-Math.PI / 2;
-
-            /// <summary>
-            /// Максимальное значение для начальной инициализации весов
-            /// </summary>
-            private static double _initMaxWeight = 1;//Math.PI / 2;
-
-            /// <summary>
-            /// Количество узлов на предыдущем слое
-            /// </summary>
-            private int _inputLayerSize;
-
-            /// <summary>
-            /// Вектор входных весов нейрона
-            /// </summary>
-            private double[] _weights;
-
-            /// <summary>
-            /// Вес на сигнале поляризации
-            /// </summary>
-            private double _biasWeight = 0.01;
-
-            /// <summary>
-            /// Ссылка на предыдущий слой нейронов 
-            /// </summary>
-            private Node[] _prevLayer;
-
-            /// <summary>
-            /// Фиктивный нейрон
-            /// </summary>
-            public Node(Node[] prevLayerNodes)
+            _values = new double[structure.Length][];
+            _errors = new double[structure.Length][];
+            for (int i = 0; i < structure.Length; i++)
             {
-                _prevLayer = prevLayerNodes;
-
-                if (prevLayerNodes == null) return;
-
-                _inputLayerSize = prevLayerNodes.Length;
-                _weights = new double[_inputLayerSize];
-                for (int i = 0; i < _weights.Length; ++i)
-                    _weights[i] = _initMinWeight + _r.NextDouble() * (_initMaxWeight - _initMinWeight);
+                _errors[i] = new double[structure[i]];
+                _values[i] = new double[structure[i]+1];
+                _values[i][structure[i]] = 1;
             }
-
-            /// <summary>
-            /// Активация нейрона
-            /// </summary>
-            public void Activate()
+            _weights = new double[structure.Length - 1][,];
+            for(int n = 0; n < structure.Length-1; n++)
             {
-                _charge = _biasWeight * BiasSignal;
-                for (int i = 0; i < _prevLayer.Length; ++i)
-                    _charge += _prevLayer[i].output * _weights[i];
-                output = ActivationFunction(_charge);
-                
-                _charge = 0;
+                var rowsCount = structure[n]+1;
+                var columnsCount = structure[n+1];
+                _weights[n] = new double[rowsCount, columnsCount];
+                Random random = new Random();
+                for(int i = 0; i < rowsCount; i++)
+                {
+                    for(int j=0; j<columnsCount; j++)
+                    {
+                        _weights[n][i, j] = random.NextDouble() * (upRandomBound - lowRandomBound) + lowRandomBound;
+                    }
+                }
             }
-
-
-            /// <summary>
-            /// Распространение ошибки на предыдущий слой и пересчёт весов
-            /// </summary>
-            public void BackPropagation(double learningRate)
-            {
-                error *= output * (1 - output);
-                _biasWeight += learningRate * error * BiasSignal;
-
-                for (int i = 0; i < _inputLayerSize; i++)
-                    _prevLayer[i].error += error * _weights[i];
-
-                for (int i = 0; i < _inputLayerSize; i++)
-                    _weights[i] += learningRate * error * _prevLayer[i].output;
-
-                error = 0;
-            }
-
-            /// <summary>
-            /// Функция активации
-            /// </summary>
-            private static double ActivationFunction(double value) => 1 / (1 + Math.Exp(-value));//Math.Atan(value);
         }
 
-        private const double LearningRate = 0.01;
-        private Node[] _sensors;
-        private Node[][] _layers;
-        private Node[] _outputs;
-
-        /// <summary>
-        /// Конструктор нейросети
-        /// </summary>
-        public StudentNetwork(int[] structure)
+        private double Error(double[] output)
         {
-            _layers = new Node[structure.Length][];
-
-            // Обработка входного слоя
-            _layers[0] = new Node[structure[0]];
-            for (int neuron = 0; neuron < structure[0]; ++neuron)
-                _layers[0][neuron] = new Node(null);
-            _sensors = _layers[0];
-
-            // Обработка остальных слоёв
-            for (int layer = 1; layer < structure.Length; ++layer)
+            double result = 0;
+            for(int i = 0; i < output.Length; i++)
             {
-                _layers[layer] = new Node[structure[layer]];
-                for (int neuron = 0; neuron < structure[layer]; ++neuron)
-                    _layers[layer][neuron] = new Node(_layers[layer - 1]);
+                result += (Math.Pow(output[i] - _values[_values.Length - 1][i], 2));
             }
-            _outputs = _layers[_layers.Length - 1];
+            result /= output.Length;
+            return result;
         }
 
-        /// <summary>
-        /// Прямой проход
-        /// </summary>
-        private void Run(Sample sample)
+        private void ComputeValues(double[] input)
         {
-            for (int i = 0; i < sample.input.Length; i++)
-                _sensors[i].output = sample.input[i];
-
-            for (int i = 1; i < _layers.Length; i++)
-                for (int j = 0; j < _layers[i].Length; j++)
-                    _layers[i][j].Activate();
-
-            sample.Output = new double[_layers[_layers.Length - 1].Length];
-
-            for (int i = 0; i < sample.Output.Length; i++)
-                sample.Output[i] = _layers[_layers.Length - 1][i].output;
-
-            sample.ProcessPrediction(sample.Output);
-        }
-
-        /// <summary>
-        /// Прямой проход
-        /// </summary>
-        private double[] Run(double[] input)
-        {
-            for (int i = 0; i < input.Length; i++)
-                _sensors[i].output = input[i];
-
-            for (int i = 1; i < _layers.Length; i++)
-                for (int j = 0; j < _layers[i].Length; j++)
-                    _layers[i][j].Activate();
-
-            var output = new double[_layers[_layers.Length - 1].Length];
-
-            for (int i = 0; i < _layers[_layers.Length - 1].Length; i++)
-                output[i] = _layers[_layers.Length - 1][i].output;
-
-            return output;
-        }
-
-        /// <summary>
-        /// Обратное распространение ошибки
-        /// </summary>
-        private void BackPropagation(Sample sample)
-        {
-            for (int i = 0; i < _layers[_layers.Length - 1].Length; i++)
-                _layers[_layers.Length - 1][i].error = sample.error[i];
-
-            var board = _layers.Length - 1;
-            Parallel.For(0, board, t =>
+            for(int j = 0; j < input.Length; j++)
             {
-                for (int i = _layers.Length - 1; i >= 0; --i)
-                    for (int j = 0; j < _layers[i].Length; ++j)
-                        _layers[i][j].BackPropagation(LearningRate);
-            });
+                _values[0][j] = input[j];
+            }
+            for(int i=1; i < _values.GetLength(0); i++)
+            {
+                MultiplyAndApplySigmoid(_values[i - 1], _weights[i-1], _values[i]);
+            }
+        }
+
+        private static void MultiplyAndApplySigmoid(double[] vector, double [,] matrix, double[] result)
+        {
+            var rowsCount = matrix.GetLength(0);
+            var colCount = matrix.GetLength(1);
+            for (int i = 0; i <colCount ; i++)
+            {
+                double sum = 0;
+                for(int j = 0; j <rowsCount; j++)
+                {
+                    sum += vector[j] * matrix[j, i];
+                }
+                result[i] = Sigmoid(sum);
+            }  
+        }
+
+        private static double Sigmoid(double value)
+        {
+            return 1.0 / (Math.Exp(-value) + 1);
+        }
+
+        private void ComputeErrors(double[] output)
+        {
+            for (var j = 0; j < output.Length; j++)
+            {
+                var actualValue = _values[_errors.Length - 1][j];
+                var expectedValue = output[j];
+                _errors[_errors.Length - 1][j] = actualValue * (1 - actualValue) * (expectedValue - actualValue);
+            }
+            for (int i = _errors.Length - 2; i >= 1; i--)
+            {
+                for (int j = 0; j < _errors[i].Length; j++)
+                {
+                    var value = _values[i][j];
+                    value = value * (1 - value);
+                    var sum = 0.0;
+                    for (int k = 0; k < _errors[i + 1].Length; k++)
+                    {
+                        sum += _errors[i + 1][k] * _weights[i][j, k];
+                    }
+                    _errors[i][j] = value * sum;
+                }
+            }
+        }
+
+        private void ComputeWeights()
+        {
+           
+            for(int n = 0; n < _weights.Length; n++)
+            {
+                for(int i = 0; i < _weights[n].GetLength(0); i++)
+                {
+                    for(int j = 0; j < _weights[n].GetLength(1); j++)
+                    {
+                        var dWeight = Speed * _errors[n + 1][j] * _values[n][i];
+                        _weights[n][i, j] += dWeight;
+                    }
+                }
+            }
         }
 
         public override int Train(Sample sample, double acceptableError, bool parallel)
         {
-            int i = 0;
-            while (i < 100)
+            int iters = 1;
+            while (Error(sample.Output) > acceptableError)
             {
-                Run(sample);
-                
-                if (sample.EstimatedError() < acceptableError && sample.Correct())
-                    return i;
-
-                BackPropagation(sample);
-                ++i;
+                ComputeValues(sample.input);
+                ComputeErrors(sample.Output);
+                ComputeWeights();
+                ++iters;
             }
-            return i;
+
+            return iters;
         }
 
         public override double TrainOnDataSet(SamplesSet samplesSet, int epochsCount, double acceptableError, bool parallel)
         {
-            double accuracy = 0;
-            int samplesLooked = 0;
-            double samplesCount = samplesSet.samples.Count * epochsCount;
-            var startTime = DateTime.Now;
-            while (epochsCount > 0)
+            //  Сначала надо сконструировать массивы входов и выходов
+            double[][] inputs = new double[samplesSet.Count][];
+            double[][] outputs = new double[samplesSet.Count][];
+
+            //  Теперь массивы из samplesSet группируем в inputs и outputs
+            for (int i = 0; i < samplesSet.Count; ++i)
             {
-                double samplesCorrect = 0;
-                for (int i = 0; i < samplesSet.samples.Count; i++)
-                {
-                    if (Train(samplesSet.samples.ElementAt(i), acceptableError, parallel) == 0)
-                        ++samplesCorrect;
-                    ++samplesLooked;
-                    if (samplesLooked % 25 == 0)
-                        OnTrainProgress(samplesLooked / samplesCount, accuracy, DateTime.Now - startTime);
-                }
-                accuracy = samplesCorrect / samplesSet.samples.Count;
-                if (accuracy >= 1 - acceptableError - 1e-10)
-                {
-                    OnTrainProgress(1, accuracy, DateTime.Now - startTime);
-                    return accuracy;
-                }
-                OnTrainProgress(samplesLooked / samplesCount, accuracy, DateTime.Now - startTime);
-                --epochsCount;
+                inputs[i] = samplesSet[i].input;
+                outputs[i] = samplesSet[i].Output;
             }
-            OnTrainProgress(1, accuracy, DateTime.Now - startTime);
-            return accuracy;
+
+            //  Текущий счётчик эпох
+            int epochToRun = 0;
+
+            double error = double.PositiveInfinity;
+
+#if DEBUG
+            StreamWriter errorsFile = File.CreateText("errors.csv");
+#endif
+
+            _stopWatch.Restart();
+
+            while (epochToRun < epochsCount && error > acceptableError)
+            {
+                epochToRun++;
+                error = 0;
+                for (int i = 0; i < inputs.Length; i++)
+                {
+                    ComputeValues(inputs[i]);
+                    ComputeErrors(outputs[i]);
+                    ComputeWeights();
+                    error += Error(outputs[i]);
+                }
+                error /= inputs.Length;
+                errorsFile.WriteLine(error);
+                OnTrainProgress(epochToRun * 1.0 / epochsCount, error, _stopWatch.Elapsed);
+            }
+
+#if DEBUG
+            errorsFile.Close();
+#endif
+            OnTrainProgress(epochToRun * 1.0 / epochsCount, error, _stopWatch.Elapsed);
+            _stopWatch.Stop();
+            return error;
         }
 
-        protected override double[] Compute(double[] input) => Run(input);
+        protected override double[] Compute(double[] input)
+        {
+            ComputeValues(input);
+            return _values.Last().Take(_values.Last().Length - 1).ToArray();
+        }
     }
 }
